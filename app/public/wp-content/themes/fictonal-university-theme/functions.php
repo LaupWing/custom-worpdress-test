d03ae21d96ae9015b594f9e3fd32cc3552a13408
LaupWing <loc_strict@hotmail.com>
2023-04-25 05:03:06 +0200

    udpate

--- app/public/wp-content/themes/fictonal-university-theme/functions.php
+++ app/public/wp-content/themes/fictonal-university-theme/functions.php
@@ -5,6 +5,4 @@ function university_files() {
 }
 
 add_action("wp_enqueue_scripts", "university_files");
-
-
 ?>
--- app/public/wp-content/themes/fictonal-university-theme/header.php
+++ app/public/wp-content/themes/fictonal-university-theme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <?php wp_head() ?>
+   <?php wp_head(); ?>
 </head>
 <body>
    
