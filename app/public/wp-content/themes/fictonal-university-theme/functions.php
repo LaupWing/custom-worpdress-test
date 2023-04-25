81d68ff0085d3a668cf3dcbb4526ba8c32e79636
LaupWing <loc_strict@hotmail.com>
2023-04-25 05:29:49 +0200

    it works

--- app/public/wp-content/themes/fictonal-university-theme/functions.php
+++ app/public/wp-content/themes/fictonal-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 function university_files() {
+   wp_enqueue_style("font-awesome", "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
    wp_enqueue_style("our-main-styles", get_theme_file_uri("/build/style-index.css"));
 }
 
