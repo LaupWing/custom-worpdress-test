0660b1149d023dfec3e28d62aba7afad7c3276a0
LaupWing <loc_strict@hotmail.com>
2023-04-28 16:21:30 +0200

    it works now

--- app/public/wp-content/themes/fictonal-university-theme/index.php
+++ app/public/wp-content/themes/fictonal-university-theme/index.php
@@ -1,7 +1,9 @@
-<?php get_header() ?>
+<?php
+get_header();
+?>
 
 <div class="page-banner">
-   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
+   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/images/library-hero.jpg") ?>)"></div>
    <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome!</h1>
       <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -72,7 +74,7 @@
 <div class="hero-slider">
    <div data-glide-el="track" class="glide__track">
       <div class="glide__slides">
-         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bus.jpg') ?>)">
+         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("/images/bus.jpg") ?>)">
             <div class="hero-slider__interior container">
                <div class="hero-slider__overlay">
                   <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -81,7 +83,7 @@
                </div>
             </div>
          </div>
-         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/apples.jpg') ?>)">
+         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("/images/apples.jpg") ?>)">
             <div class="hero-slider__interior container">
                <div class="hero-slider__overlay">
                   <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -90,7 +92,7 @@
                </div>
             </div>
          </div>
-         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg') ?>)">
+         <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri("/images/bread.jpg") ?>)">
             <div class="hero-slider__interior container">
                <div class="hero-slider__overlay">
                   <h2 class="headline headline--medium t-center">Free Food</h2>
@@ -104,4 +106,6 @@
    </div>
 </div>
 
-<?php get_footer() ?>
+<?php
+get_footer();
+?>
