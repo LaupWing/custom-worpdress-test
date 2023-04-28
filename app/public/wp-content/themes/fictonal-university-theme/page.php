97640ac823499b4b125c022e4c40abd7e94e11b2
LaupWing <loc_strict@hotmail.com>
2023-04-28 13:17:31 +0200

    udpate

--- app/public/wp-content/themes/fictonal-university-theme/page.php
+++ app/public/wp-content/themes/fictonal-university-theme/page.php
@@ -16,11 +16,12 @@
 
    <div class="container container--narrow page-section">
       <?php 
-         if(wp_get_post_parent_id(get_the_ID())){
+         $parentId = wp_get_post_parent_id(get_the_ID());
+         if($parentId){
       ?>
          <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
-               <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span>
+               <a class="metabox__blog-home-link" href="<?php echo get_permalink($parentId) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentId) ?></a> <span class="metabox__main"><?php the_title() ?></span>
             </p>
          </div>
       <?php
