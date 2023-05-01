a4499bd9803accd436304fe7b846482f269cc067
LaupWing <loc_strict@hotmail.com>
2023-05-01 09:12:47 +0200

    36

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -15,6 +15,22 @@ function university_post_types() {
       ],
       "menu_icon" => "dashicons-calendar"
    ]);
+
+   register_post_type("program", [
+      "rewrite" => ["slug" => "programs"],
+      "has_archive" => true,
+      "show_in_rest" => true,
+      "supports" => ["title", "editor"],
+      "public" => true,
+      "labels" => [
+         "name" => "Programs",
+         "add_new_item" => "Add New Program",
+         "edit_item" => "Edit Program",
+         "all_items" => "All Programs",
+         "singular_name" => "Program"
+      ],
+      "menu_icon" => "dashicons-awards"
+   ]);
 }
 
 add_action("init", "university_post_types");
