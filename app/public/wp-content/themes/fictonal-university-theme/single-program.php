e4af093284430dc234ce7ff6e14f73d7646d18db
LaupWing <loc_strict@hotmail.com>
2023-05-01 10:54:02 +0200

    thumbnails

--- app/public/wp-content/themes/fictonal-university-theme/single-program.php
+++ app/public/wp-content/themes/fictonal-university-theme/single-program.php
@@ -50,7 +50,7 @@
       ?>
          <li class="professor-card__list-item">
             <a class="professor-card" href="<?php the_permalink() ?>">
-               <img src="<?php the_post_thumbnail_url(); ?>" class="professor-card__image" alt="">
+               <img src="<?php the_post_thumbnail_url("professorLandscape"); ?>" class="professor-card__image" alt="">
                <span class="professor-card__name">
                   <?php echo the_title(); ?>
                </span>
