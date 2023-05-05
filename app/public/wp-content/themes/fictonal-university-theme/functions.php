7f9ebfecc1a9d77e597ebefc20c0ee5364f74a6f
LaupWing <loc_strict@hotmail.com>
2023-05-05 05:52:06 +0200

    update

--- app/public/wp-content/themes/fictonal-university-theme/functions.php
+++ app/public/wp-content/themes/fictonal-university-theme/functions.php
@@ -5,9 +5,9 @@ add_action("rest_api_init", "university_custom_rest");
 function university_custom_rest() {
    register_rest_field("post", "authorName", [
       "get_callback" => function() {
-         
+         return get_the_author();
       }
-   ]);  
+   ]);
 }
 
 function pageBanner($args = NULL){
