02dc4d43ed1e74ca1869e8345205275d1db4c55c
LaupWing <loc_strict@hotmail.com>
2023-05-05 09:31:13 +0200

    76

--- app/public/wp-content/themes/fictonal-university-theme/includes/search-route.php
+++ app/public/wp-content/themes/fictonal-university-theme/includes/search-route.php
@@ -10,20 +10,58 @@ function universityRegisterSearch(){
 }
 
 function universitySerachResults($data) {
-   $proffessors = new WP_Query([
-      "post_type" => "professor",
+   $mainQuery = new WP_Query([
+      "post_type" => ["professor", "page", "post", "event", "campus", "program"],
       "s" =>  sanitize_text_field($data["term"])
    ]);
 
-   $proffessorResults = [];
-   while($proffessors->have_posts()) {
-      $proffessors->the_post();
-      array_push($proffessorResults, [
-         "title" => get_the_title(),
-         "id" => get_the_ID(),
-         "permalink" => get_the_permalink()
-      ]);
+   $results = [
+      "generalInfo" => [],
+      "professors" => [],
+      "programs" => [],
+      "events" => [],
+      "campuses" => []
+   ];
+
+   while($mainQuery->have_posts()) {
+      $mainQuery->the_post();
+      
+      if(get_post_type() == "post" OR get_post_type() == "page"){
+         array_push($results["generalInfo"], [
+            "title" => get_the_title(),
+            "id" => get_the_ID(),
+            "permalink" => get_the_permalink()
+         ]);
+      }
+      if(get_post_type() == "professor"){
+         array_push($results["professors"], [
+            "title" => get_the_title(),
+            "id" => get_the_ID(),
+            "permalink" => get_the_permalink()
+         ]);
+      }
+      if(get_post_type() == "program"){
+         array_push($results["programs"], [
+            "title" => get_the_title(),
+            "id" => get_the_ID(),
+            "permalink" => get_the_permalink()
+         ]);
+      }
+      if(get_post_type() == "event"){
+         array_push($results["events"], [
+            "title" => get_the_title(),
+            "id" => get_the_ID(),
+            "permalink" => get_the_permalink()
+         ]);
+      }
+      if(get_post_type() == "campus"){
+         array_push($results["campuses"], [
+            "title" => get_the_title(),
+            "id" => get_the_ID(),
+            "permalink" => get_the_permalink()
+         ]);
+      }
    } 
 
-   return $proffessorResults;
+   return $results;
 }
