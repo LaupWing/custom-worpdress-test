f236dcbb53752df77429d8aeb3f0bd74fc49cd52
LaupWing <loc_strict@hotmail.com>
2023-05-08 10:12:55 +0200

    redicret

--- app/public/wp-content/themes/fictonal-university-theme/page-my-notes.php
+++ app/public/wp-content/themes/fictonal-university-theme/page-my-notes.php
@@ -1,4 +1,10 @@
 <?php
+
+if(!is_user_logged_in()) {
+   wp_redirect(esc_url(site_url("/")));
+   exit;
+}
+
 get_header();
 
 while (have_posts()) {
@@ -6,7 +12,7 @@ while (have_posts()) {
    pageBanner();
 ?>
    <div class="container container--narrow page-section">
-      
+
    </div>
 <?php }
 
