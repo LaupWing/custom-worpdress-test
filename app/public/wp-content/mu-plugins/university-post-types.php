f7b1b67f22d9cadc2efe4211e66b876af1bcae6f
LaupWing <loc_strict@hotmail.com>
2023-05-08 10:16:10 +0200

    udpate

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -50,6 +50,22 @@ function university_post_types() {
       "menu_icon" => "dashicons-welcome-learn-more"
    ]);
 
+   register_post_type("note", [
+      "has_archive" => true,
+      "show_in_rest" => true,
+      "supports" => ["title", "editor"],
+      "public" => false,
+      "show_ui" => true,
+      "labels" => [
+         "name" => "Notes",
+         "add_new_item" => "Add New Note",
+         "edit_item" => "Edit Note",
+         "all_items" => "All Notes",
+         "singular_name" => "Note"
+      ],
+      "menu_icon" => "dashicons-welcome-write-blog"
+   ]);
+
    register_post_type("campus", [
       "capability_type" => "campus",
       "map_meta_cap" => true,
