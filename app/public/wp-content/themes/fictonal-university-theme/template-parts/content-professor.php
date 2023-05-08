61a212dceec1fc58319d227ec9bb20586ef2efe3
LaupWing <loc_strict@hotmail.com>
2023-05-08 09:08:10 +0200

    conett page

--- app/public/wp-content/themes/fictonal-university-theme/template-parts/content-professor.php
+++ app/public/wp-content/themes/fictonal-university-theme/template-parts/content-professor.php
@@ -1,8 +1,10 @@
-<li class="professor-card__list-item">
-   <a class="professor-card" href="<?php the_permalink() ?>">
-      <img src="<?php the_post_thumbnail_url("professorLandscape"); ?>" class="professor-card__image" alt="">
-      <span class="professor-card__name">
-         <?php echo the_title(); ?>
-      </span>
-   </a>
-</li>   
+<div class="post-item">
+   <li class="professor-card__list-item">
+      <a class="professor-card" href="<?php the_permalink() ?>">
+         <img src="<?php the_post_thumbnail_url("professorLandscape"); ?>" class="professor-card__image" alt="">
+         <span class="professor-card__name">
+            <?php echo the_title(); ?>
+         </span>
+      </a>
+   </li>   
+</div>
