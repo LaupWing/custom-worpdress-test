10dd54c4e104b5f72b614d68ebe6eec4b6e3eaa3
LaupWing <loc_strict@hotmail.com>
2023-05-10 09:12:33 +0200

    105

--- app/public/wp-content/themes/fictonal-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictonal-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '5a41e6a30f7b42c7c8d3');
+<?php return array('dependencies' => array('jquery'), 'version' => '1547d26cf121b8e2d62d');
