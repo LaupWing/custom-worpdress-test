69b7aed924de7915a48c2ae589e3ae02146596d0
LaupWing <loc_strict@hotmail.com>
2023-05-11 15:23:51 +0200

    update

--- app/public/wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ app/public/wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -12,10 +12,15 @@ class WordCountAndTimePlugin {
    function __construct()
    {
       add_action("admin_menu", array($this, "adminPage"));
+      add_action("admin_init", array($this, "settings"));
    }
    
    function adminPage(){
       add_options_page("Word Count Settings", "Word Count", "manage_options", "word-count-settings-page", array($this, "ourHTML"));
+   }
+
+   function settings(){
+      register_setting("wordcountplugin", "wcp_location", array("sanitize_callback" => "sanitize_text_field", "default" => 0));
    }
    
    function ourHTML() {
