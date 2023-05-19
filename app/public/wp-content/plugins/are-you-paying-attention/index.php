6a5becbd4c5b6f0a547aaf9d3950049b7bbf5bd6
LaupWing <loc_strict@hotmail.com>
2023-05-19 11:56:38 +0200

    130

---
+++ app/public/wp-content/plugins/are-you-paying-attention/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+   Plugin Name: Are You Paying Attention Quiz
+   Description: Give your readers a multiple choice question.
+   Version: 1.0
+   Author: Laupwing
+   Author URI: 
+*/
+
+if (!defined("ABSPATH")){
+   exit;
+}
+
+class AreYouPayingAttention {
+   function __construct()
+   {
+      add_action("enqueue_block_editor_assets", array($this, "adminAssets"));
+   }
+
+   function adminAssets() {
+      wp_enqueue_script("ournewblogtype", plugin_dir_url(__FILE__) . "test.js", array("wp-blocks", "wp-element"));
+   }
+}
+
+$areYouPayingAttention = new AreYouPayingAttention();
