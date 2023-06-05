c59b0e7a00d1bde682406c1a4837d431181cd46d
LaupWing <loc_strict@hotmail.com>
2023-06-05 08:11:23 +0200

    udpate

--- app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '08f9d50be87934c01ea7');
+<?php return array('dependencies' => array('wp-element'), 'version' => '91758c616d7f99871501');
--- app/public/wp-content/plugins/are-you-paying-attention/src/index.js
+++ app/public/wp-content/plugins/are-you-paying-attention/src/index.js
@@ -1,51 +1,4 @@
 // import { TextControl } from "@wordpress/components"
-
-wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
-   title: "Are You Paying Attention?",
-   icon: "smiley",
-   category: "common",
-   attributes: {
-      skyColor: {
-         type: "string",
-      },
-      grassColor: {
-         type: "string"
-      }
-   }, 
-   edit: (props) => {
-      const updateSkyColor = (e) => {
-         props.setAttributes({
-            skyColor: e.target.value
-         })
-      }
-      const updateGrassColor = (e) => {
-         props.setAttributes({
-            grassColor: e.target.value
-         })
-      }
-      
-      return (
-         <div>
-            <input 
-               type="text" 
-               placeholder="sky color" 
-               value={props.attributes.skyColor}
-               onChange={updateSkyColor}
-            />
-            <input 
-               type="text" 
-               placeholder="grass color" 
-               value={props.attributes.grassColor}
-               onChange={updateGrassColor}
-            />
-         </div>
-      )
-   },
-   save: (props) => {
-      return null
-   }
-})
-
 const Edit = (props) => {
    const updateSkyColor = (e) => {
       props.setAttributes({
@@ -60,7 +13,37 @@ const Edit = (props) => {
    
    return (
       <div>
-         <input type="text" />
+         <input 
+            type="text" 
+            placeholder="sky color" 
+            value={props.attributes.skyColor}
+            onChange={updateSkyColor}
+         />
+         <input 
+            type="text" 
+            placeholder="grass color" 
+            value={props.attributes.grassColor}
+            onChange={updateGrassColor}
+         />
       </div>
    )
 }
+
+wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
+   title: "Are You Paying Attention?",
+   icon: "smiley",
+   category: "common",
+   attributes: {
+      skyColor: {
+         type: "string",
+      },
+      grassColor: {
+         type: "string"
+      }
+   }, 
+   edit: Edit,
+   save: () => {
+      return null
+   }
+})
+
