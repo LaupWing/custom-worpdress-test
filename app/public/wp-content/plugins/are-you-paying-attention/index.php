cddd7e72f36c123ee5ad5d62f9f0aa66bd1b2599
LaupWing <loc_strict@hotmail.com>
2023-06-05 06:58:28 +0200

    udpate

--- app/public/wp-content/plugins/are-you-paying-attention/index.php
+++ app/public/wp-content/plugins/are-you-paying-attention/index.php
@@ -16,17 +16,28 @@ class AreYouPayingAttention {
    function __construct()
    {
       add_action(
-         "enqueue_block_editor_assets", 
+         "init", 
          array($this, "adminAssets")
       );
    }
 
    function adminAssets() {
-      wp_enqueue_script(
+      wp_register_script(
          "ournewblogtype", 
          plugin_dir_url(__FILE__) . "build/index.js", 
          array("wp-blocks", "wp-element")
       );
+      register_block_type("ourplugin/are-you-paying-attention", array(
+         "editor_script" => "ournewblogtype",
+         "render_callback" => array($this, "theHTML")
+      ));
+   }
+
+   function theHTML($attributes) {
+      ob_start(); 
+   ?>
+      <h3>Today the sky sis blue <?php echo esc_html($attributes["skyColor"]) ?> and the grass is <?php echo esc_html($attributes["grassColor"]) ?> </h3>
+   <?php return ob_get_clean();
    }
 }
 
