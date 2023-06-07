d1ebdeb9f4d610b0698c28151915e919f468ea27
LaupWing <loc_strict@hotmail.com>
2023-06-07 08:46:24 +0200

    udpate

--- app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '6418a1cf0acbdf850065');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '72b97e30605df9ab37e4');
