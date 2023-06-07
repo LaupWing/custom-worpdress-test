92db4f589430f705f8d657dae3ae28c919582d98
LaupWing <loc_strict@hotmail.com>
2023-06-07 12:43:08 +0200

    150

---
+++ app/public/wp-content/plugins/featured-professor/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => 'a1c964f1a4cdef6429c2');
--- app/public/wp-content/plugins/featured-professor/src/index.js
+++ app/public/wp-content/plugins/featured-professor/src/index.js
@@ -1,25 +1,34 @@
 import "./index.scss"
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
-  title: "Professor Callout",
-  description: "Include a short description and link to a professor of your choice",
-  icon: "welcome-learn-more",
-  category: "common",
-  edit: EditComponent,
-  save: function () {
-    return null
-  }
+   title: "Professor Callout",
+   description:
+      "Include a short description and link to a professor of your choice",
+   icon: "welcome-learn-more",
+   category: "common",
+   attributes: {
+      profId: {
+         type: "string"
+      }
+   },
+   edit: EditComponent,
+   save: function () {
+      return null
+   },
 })
 
-function EditComponent() {
-  return (
-    <div className="featured-professor-wrapper">
-      <div className="professor-select-container">
-        We will have a select dropdown form element here.
+function EditComponent(props) {
+   return (
+      <div className="featured-professor-wrapper">
+         <div className="professor-select-container">
+            <select onChange={e => props.setAttributes({ profId: e.target.value})}>
+               <option value="">Select a professor</option>
+               <option value="1" selected={props.attributes.profId  ==1 }>1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+            </select>
+         </div>
+         <div>The HTML preview of the selected professor will appear here.</div>
       </div>
-      <div>
-        The HTML preview of the selected professor will appear here.
-      </div>
-    </div>
-  )
+   )
 }
