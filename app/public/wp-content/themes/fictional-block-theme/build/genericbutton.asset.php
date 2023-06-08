b40d7e9d4693b7cca9b28173cb764b21f69747f3
LaupWing <loc_strict@hotmail.com>
2023-06-08 19:02:51 +0200

    183

--- app/public/wp-content/themes/fictional-block-theme/build/genericbutton.asset.php
+++ app/public/wp-content/themes/fictional-block-theme/build/genericbutton.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c62f4cf4887d19aef4e4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element'), 'version' => '124598f0ee45cdf05032');
