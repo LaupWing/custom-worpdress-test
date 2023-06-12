d1622eb5c82994f7f82103d9d723968b484a0c5e
LaupWing <loc_strict@hotmail.com>
2023-06-12 10:10:19 +0200

    187

--- app/public/wp-content/themes/fictional-block-theme/our-blocks/banner.php
+++ app/public/wp-content/themes/fictional-block-theme/our-blocks/banner.php
@@ -1,11 +1,11 @@
-<div className="page-banner">
+<div class="page-banner">
    <div 
-      className="page-banner__bg-image" 
-      style="background-image: url('<?php echo get_theme_file_uri("/images/library-hero.jpgzzzz") ?>')"
+      class="page-banner__bg-image" 
+      style="background-image: url('<?php echo get_theme_file_uri("/images/library-hero.jpg") ?>')"
    >
 
    </div>
-   <div className="page-banner__content container t-center c-white">
+   <div class="page-banner__content container t-center c-white">
       <?php echo $content; ?>
    </div>
 </div>
